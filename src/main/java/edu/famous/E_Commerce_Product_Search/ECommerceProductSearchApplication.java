02ebeca4b3983b92405db6d04523e6516374efe3
CharakaGunathilake <gunathilakec122@gmail.com>
2025-08-07 20:39:44 +0530

    Inital project setup

---
+++ src/main/java/edu/famous/E_Commerce_Product_Search/ECommerceProductSearchApplication.java
@@ -0,0 +1,13 @@
+package edu.famous.E_Commerce_Product_Search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceProductSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceProductSearchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/famous/E_Commerce_Product_Search/ECommerceProductSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.famous.E_Commerce_Product_Search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceProductSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
